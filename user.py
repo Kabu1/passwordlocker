d813d07c564dc189e424f39e20ca5a2cb19943c8
kabu1 <kkabubenson@gmail.com>
2020-09-04 08:22:35 -0400

    add instance variables and user list variable

--- user.py
+++ user.py
@@ -2,7 +2,11 @@ class User:
     """
     Class that creates user accounts and their details.
     """
-def __init__(self, first_name, last_name, password):
-    """
-    Defining the properties of our objects
-    """
+    user_list = [] #empty contact list
+    def __init__(self, first_name, last_name, password):
+        """
+        Defining the properties of our objects
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password

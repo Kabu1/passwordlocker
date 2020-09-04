6f47fc360e62efe3e158b7e635bf83cb28546419
kabu1 <kkabubenson@gmail.com>
2020-09-04 09:32:44 -0400

    create a new user

--- user_credential_test.py
+++ user_credential_test.py
@@ -1,3 +1,16 @@
 import unittest #importing unittest module
 from user import User #importing the user class
 from credentials import Credentials #importing the credentials class
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class 
+
+    Args: 
+        unittest.TestCase: helps in creating test cases
+    '''
+        def setUp(self):
+            '''
+            Set up method that runs before each test case is conducted.
+            '''
+            self.new_user = User("Test","User","Pass@2020")

2bea27640b27098dfcba466fe8600cf77929dba6
kabu1 <kkabubenson@gmail.com>
2020-09-04 07:57:11 -0400

    defining properties of the user using init

---
+++ user.py
@@ -0,0 +1,8 @@
+class User:
+    """
+    Class that creates user accounts and their details.
+    """
+def __init__(self, first_name, last_name, password):
+    """
+    Defining the properties of our objects
+    """

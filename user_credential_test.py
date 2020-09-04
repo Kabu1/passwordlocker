dd4943544be061c1d447a62a4efd9873df2c1507
kabu1 <kkabubenson@gmail.com>
2020-09-04 10:08:36 -0400

    test initialization

--- user_credential_test.py
+++ user_credential_test.py
@@ -1,6 +1,5 @@
 import unittest #importing unittest module
 from user import User #importing the user class
-from credentials import Credentials #importing the credentials class
 
 class TestUser(unittest.TestCase):
     '''
@@ -9,8 +8,17 @@ class TestUser(unittest.TestCase):
     Args: 
         unittest.TestCase: helps in creating test cases
     '''
-        def setUp(self):
-            '''
-            Set up method that runs before each test case is conducted.
-            '''
-            self.new_user = User("Test","User","Pass@2020")
+    def setUp(self):
+        '''
+        Set up method that runs before each test case is conducted.
+        '''
+        self.new_user = User("Test","User","Pass@2020")
+    def test__init(self):
+        '''
+        Test to check if the initialization of user instances is properly done
+        '''
+        self.assertEqual(self.new_user.first_name,"Test")
+        self.assertEqual(self.new_user.last_name,"User")
+        self.assertEqual(self.new_user.password,"Pass@2020")
+if __name__ == '__main__':
+    unittest.main()

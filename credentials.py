c897d7c2ec626357cf0265fd1d03697db8787c4b
kabu1 <kkabubenson@gmail.com>
2020-09-05 04:46:37 -0400

    initializing credentials properties

--- credentials.py
+++ credentials.py
@@ -1,3 +1,15 @@
 import string
 import pyperclip
 import random
+
+class Credential:
+    '''
+    class to create users credentials i.e generate passwords and save their information
+    '''
+    credentials_list = []
+
+    def __init__(self, user_name, acc_name, email, acc_password):
+        '''
+        defining the properties of object credential
+        '''
+    
